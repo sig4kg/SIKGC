3e367907eb2aad32edfc7dd320680014db0a3684
sylviawangfr <newtyjun@hotmail.com>
2021-06-01 00:21:07 +0800

    Initial commit

--- Kemcode/FixTheFormatOfKB2E.py
+++
@@ -1,8 +0,0 @@
-a = open("_Ev6.4RuleFlow2/2ndIteration/2ndPBRChecking/newTriplescheck.txt")
-b = open("_Ev6.4RuleFlow2/2ndIteration/2ndPBRChecking/newTriplescheckFix.txt","w")
-
-for line in a:
-	row = line.split()
-	b.write(row[0]+"\t"+row[2]+"\t"+row[1]+"\n")
-
-b.close()

10a767facd9e81e72e38570f9cad7f9c88ecd486
sylviawangfr <newtyjun@hotmail.com>
2021-06-06 15:54:02 +0800

    refactor pipeline and fix bugs

--- pipelines/pipeline_C_R_C.py
+++ pipelines/pipeline_C_R_C.py
@@ -1,6 +1,6 @@
 from abox_scanner import abox_utils
 from abox_scanner.AboxScannerScheduler import AboxScannerScheduler
-from abox_scanner.abox_utils import ContextResources, TBOX_PATTERNS_PATH, ORIGINAL_TRIPLES_PATH, read_scanned_2_context_df
+from abox_scanner.abox_utils import TBOX_PATTERNS_PATH, ORIGINAL_TRIPLES_PATH, read_scanned_2_context_df, wait_until_file_is_saved
 import pandas as pd
 from scripts import run_scripts
 import os
@@ -14,33 +14,42 @@ def c_r_c(input_hrt_triple_file, work_dir, max_epoch=2):
     abox_scanner_scheduler.register_pattern([1, 2])
     # first round scan, get ready for training
     abox_scanner_scheduler.scan_patterns(work_dir=work_dir)
-    abox_utils.wait_until_file_is_saved(f"{work_dir}valid_hrt.txt", 60)
+    wait_until_file_is_saved(f"{work_dir}valid_hrt.txt", 60)
     read_scanned_2_context_df(work_dir, context_resource)
-    hrt_int_df_2_hrt_rumis(context_resource, work_dir + "ideal.data.txt")
     for ep in trange(max_epoch, colour="green", position=0, leave=True, desc="Pipeline processing"):
+        # context int to rumis train
+        hrt_int_df_2_hrt_rumis(context_resource, work_dir + "idea.data.txt")
+        wait_until_file_is_saved(work_dir + "ideal.data.txt", 120)
         run_scripts.run_rumis(work_dir)
+        wait_until_file_is_saved(work_dir + "DLV/extension.opm.kg.pos.10.needcheck", 60)
+        wait_until_file_is_saved(work_dir + "DLV/extension.opm.kg.neg.10.needcheck", 60)
+
         # consistency checking for new triples
         new_hrt_df1 = read_hrt_rumis_2_hrt_int_df(work_dir + "DLV/extension.opm.kg.pos.10.needcheck", context_resource)
         new_hrt_df2 = read_hrt_rumis_2_hrt_int_df(work_dir + "DLV/extension.opm.kg.neg.10.needcheck", context_resource)
         new_hrt_df = pd.concat([new_hrt_df1, new_hrt_df2], 0)
         abox_scanner_scheduler.set_triples_int_df(new_hrt_df).scan_patterns(work_dir=work_dir)
+
         # get valid new triples
-        if abox_utils.wait_until_file_is_saved(work_dir + "valid_hrt.txt", 60):
+        if abox_utils.wait_until_file_is_saved(work_dir + "valid_hrt.txt", 120):
             new_hrt_df = abox_utils.read_hrt_2_df(work_dir + "valid_hrt.txt")
         else:
             print("saving time out, exit")
+
         # check rate
         # new_count = new_hrt_df.count()
         # if new_count / train_count < 0.001:
         #     break
+
         # add new valid hrt to train set
         old_hrt_df = context_resource.hrt_tris_int_df
         train_hrt_df = pd.concat([old_hrt_df, new_hrt_df], axis=0)
+
         #  backup and clean last round data
         run_scripts.clean_rumis(work_dir=work_dir)
-        # overwrite train data
-        hrt_int_df_2_hrt_rumis(train_hrt_df, context_resource, work_dir + "idea.data.txt")
 
+        # overwrite train data in context
+        context_resource.hrt_tris_int_df = train_hrt_df
 
 
 if __name__ == "__main__":

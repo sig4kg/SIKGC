4f5c2d70d104f4c743f1e22d78b84d2d7783dde1
sylviawangfr <newtyjun@hotmail.com>
2021-08-05 15:54:18 +0800

    fix panda

--- data_utils/dataset_creator.py
+++ data_utils/dataset_creator.py
@@ -80,20 +80,36 @@ def nell_ent_to_description(data_files, output_dir):
             f2.write(f"{key}\t{ent2Category[key]}\n")
 
 
+def read_file_to_dict(filename):
+    result = dict()
+    with open(filename) as f:
+        lines = f.readlines()
+        for l in lines:
+            items = l.split('\t')
+            key = items[0].strip()
+            value = items[1].strip()
+            result.update({key: value})
+    return result
+
+
 def nell_tidyup_text_files(work_dir):
     nell_tris = pd.read_csv(work_dir + 'NELLKG0.txt', header=None, names=['head', 'relation', 'tail'], sep='\t', error_bad_lines=False, engine="python")
     all_entities = pd.merge(nell_tris[['head']].rename(columns={'head': 'entity'}),
                             nell_tris[['tail']].rename(columns={'tail': 'entity'}), how='outer').drop_duplicates(
         keep=False)
     all_relations = nell_tris[['relation']].drop_duplicates(keep=False)
-    entity_text = pd.read_csv(work_dir + 'entity2text_all.txt', header=None, names=['entity', 'text'], sep='\t', error_bad_lines=False, engine="python")
-    entity_type = pd.read_csv(work_dir + 'entity2type_all.txt', header=None, names=['entity', 'type'], sep='\t', error_bad_lines=False, engine="python")
-    rel_text = pd.read_csv(work_dir + 'relation2text_all.txt', header=None, names=['relation', 'text'], sep='\t', error_bad_lines=False, engine="python")
-    entity_longtext = pd.read_csv(work_dir + 'entity2textlong_all.txt', header=None, names=['entity', 'text'], sep='\t', error_bad_lines=False, engine="python")
-    entity2text = {row['entity']: row['text'] for _, row in entity_text.iterrows()}
-    entity2type = {row['entity']: row['type'] for _, row in entity_type.iterrows()}
-    entity2textlong = {row['entity']: row['text'] for _, row in entity_longtext.iterrows()}
-    rel2text = {row['relation']: row['text'] for _, row in rel_text.iterrows()}
+    # entity_text = pd.read_csv(work_dir + 'entity2text_all.txt', header=None, names=['entity', 'text'], sep='\t', error_bad_lines=False, engine="python")
+    # entity_type = pd.read_csv(work_dir + 'entity2type_all.txt', header=None, names=['entity', 'type'], sep='\t', error_bad_lines=False, engine="python")
+    # rel_text = pd.read_csv(work_dir + 'relation2text_all.txt', header=None, names=['relation', 'text'], sep='\t', error_bad_lines=False, engine="python")
+    # entity_longtext = pd.read_csv(work_dir + 'entity2textlong_all.txt', header=None, names=['entity', 'text'], sep='\t', error_bad_lines=False, engine="python")
+    # entity2text = {row['entity']: row['text'] for _, row in entity_text.iterrows()}
+    # entity2type = {row['entity']: row['type'] for _, row in entity_type.iterrows()}
+    # entity2textlong = {row['entity']: row['text'] for _, row in entity_longtext.iterrows()}
+    # rel2text = {row['relation']: row['text'] for _, row in rel_text.iterrows()}
+    entity2text = read_file_to_dict(work_dir + 'entity2text_all.txt')
+    entity2type = read_file_to_dict(work_dir + 'entity2type_all.txt')
+    entity2textlong = read_file_to_dict(work_dir + 'entity2textlong_all.txt')
+    rel2text = read_file_to_dict(work_dir + 'relation2text_all.txt')
     ent_text_df = all_entities[['entity']]
     ent_text_df['text'] = ent_text_df.apply(lambda row: entity2text[row.entity], axis=1)
     ent_text_df.to_csv(work_dir + "entity2text.txt", header=None, index=None, sep='\t', mode='a')

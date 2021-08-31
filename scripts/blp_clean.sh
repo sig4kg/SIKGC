36833fbbf44871a7726592754105b19a876ac3dd
sylviawangfr <newtyjun@hotmail.com>
2021-08-31 22:05:55 +0800

    fix blp_clean.sh

--- scripts/blp_clean.sh
+++ scripts/blp_clean.sh
@@ -22,10 +22,10 @@ fi
 [ -f "$WORK_DIR/valid_hrt.txt" ] && mv $WORK_DIR/valid_hrt.txt $LAST_ROUND
 [ -f "$WORK_DIR/invalid_hrt.txt" ] && mv $WORK_DIR/invalid_hrt.txt $LAST_ROUND
 [ -f "$WORK_DIR/dev-ents.txt" ] && mv $WORK_DIR/dev-ents.txt $LAST_ROUND
-[ -f "$WORK_DIR/train-ents.txt" ] && mv $WORK_DIR/train/train-ents.txt $LAST_ROUND
-[ -f "$WORK_DIR/test-ents.txt" ] && mv $WORK_DIR/train/test-ents.txt $LAST_ROUND
+[ -f "$WORK_DIR/train-ents.txt" ] && mv $WORK_DIR/train-ents.txt $LAST_ROUND
+[ -f "$WORK_DIR/test-ents.txt" ] && mv $WORK_DIR/test-ents.txt $LAST_ROUND
 [ -f "$WORK_DIR/ind-dev.tsv" ] && mv $WORK_DIR/ind-dev.tsv $LAST_ROUND
-[ -f "$WORK_DIR/ind-train.tsv" ] && mv $WORK_DIR/train/ind-train.tsv $LAST_ROUND
+[ -f "$WORK_DIR/ind-train.tsv" ] && mv $WORK_DIR/ind-train.tsv $LAST_ROUND
 [ -f "$WORK_DIR/ind-test.tsv" ] && mv $WORK_DIR/ind-test.tsv $LAST_ROUND
 #[ -f "$WORK_DIR/checkpoint/transe.ckpt" ] && mv $WORK_DIR/checkpoint/transe.ckpt $LAST_ROUND
 [ -f "$WORK_DIR/blp_new_triples.txt" ] && mv $WORK_DIR/blp_new_triples.txt $LAST_ROUND

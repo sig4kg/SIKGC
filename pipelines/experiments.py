75d1128b45e2f57e0119b19409049be4c5948e7e
sylviawangfr <newtyjun@hotmail.com>
2022-03-11 17:34:58 +0800

    fix lr

--- pipelines/experiments.py
+++ pipelines/experiments.py
@@ -129,13 +129,13 @@ def producers(dataset="TEST", work_dir="../outputs/test/", pipeline="cec", use_g
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="experiment settings")
     # dataset="TEST", work_dir="outputs/test/", pipeline="cec", use_gpu=False, loops=2
-    parser.add_argument('--dataset', type=str, default="DBpedia")
+    parser.add_argument('--dataset', type=str, default="TEST")
     parser.add_argument('--work_dir', type=str, default="../outputs/")
     parser.add_argument('--pipeline', type=str, default="clc")
     parser.add_argument('--use_gpu', type=bool, default=False)
     parser.add_argument('--loops', type=int, default=2)
     parser.add_argument("--rel_model", type=str, default="transe")
-    parser.add_argument("--inductive", type=bool, default=True)
+    parser.add_argument("--inductive", type=bool, default=False)
     args = parser.parse_args()
     producers(dataset=args.dataset,
               work_dir=args.work_dir,

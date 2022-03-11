2867d9cbe672caf98c7504f8b3e94e68b319cb2e
sylviawangfr <newtyjun@hotmail.com>
2022-03-11 12:29:47 +0800

    more epoch for db

--- pipelines/experiments.py
+++ pipelines/experiments.py
@@ -17,9 +17,11 @@ def producers(dataset="TEST", work_dir="../outputs/test/", pipeline="cec", use_g
     run_scripts.mk_dir(work_dir)
     blp_conf = BLPConfig().get_blp_config(rel_model, inductive)
     data_conf = DatasetConfig().get_config(dataset)
-    if dataset == "DBpedia":
+    if dataset == "DBpedia" and inductive:
         blp_conf['max_epochs'] = 40
         blp_conf['lr'] = 1e-5,
+    elif dataset == "DBpedia":
+        blp_conf['max_epochs'] = 30
     scores = []
     if pipeline == "cac":
         print("CRC pipeline")

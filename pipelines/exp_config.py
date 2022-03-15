32efede6b15d3deb82300ee5a98ef5252bfed56f
sylviawangfr <newtyjun@hotmail.com>
2022-03-15 22:00:33 +0800

    fix anyburl

--- pipelines/exp_config.py
+++ pipelines/exp_config.py
@@ -85,7 +85,7 @@ class BLPConfig:
             if inductive:
                 tmp_conf.update({'model': 'fasttext'})
         elif dataset == "NELL":
-            tmp_conf.update({'batch_size': 1024, 'max_epochs': 40})
+            tmp_conf.update({'batch_size': 1024, 'max_epochs': 20})
             if inductive:
                 tmp_conf.update({'lr': 1e-4})
         return tmp_conf

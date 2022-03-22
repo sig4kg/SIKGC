2cc7809fa0027687b202455d70d068463562bcc9
sylviawangfr <newtyjun@hotmail.com>
2022-03-22 11:49:39 +0800

    sample

--- pipelines/exp_config.py
+++ pipelines/exp_config.py
@@ -77,9 +77,9 @@ class BLPConfig:
             tmp_conf.update({'model': 'transductive', 'regularizer': 1e-2, 'lr': 1e-3})
 
         if dataset == "DBpedia":
-            tmp_conf.update({'max_epochs': 60})
+            tmp_conf.update({'batch_size': 1024, 'max_epochs': 30})
             if inductive:
-                tmp_conf.update({'lr': 1e-4, 'batch_size': 1024})
+                tmp_conf.update({'lr': 1e-4, 'max_epochs': 60})
         elif dataset == "TREAT":
             tmp_conf.update({'lr': 1e-3, 'regularizer': 1e-2, 'max_epochs': 50, 'batch_size': 64})
             if inductive:

4d16e9232d18072952ddae86b1244c5d15e4943b
sylviawangfr <newtyjun@hotmail.com>
2022-05-04 22:01:33 +0800

    type pred models

--- blp/type_producer.py
+++ blp/type_producer.py
@@ -1,7 +1,4 @@
-import os
-import os.path as osp
-from pathlib import Path
-from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
+from torch.utils.data import DataLoader, Dataset, SequentialSampler
 from transformers import get_linear_schedule_with_warmup
 from torch.optim import Adam
 from abox_scanner.ContextResources import ContextResources
@@ -11,14 +8,13 @@ import torch
 import numpy as np
 import utils
 import pandas as pd
-from collections import defaultdict
-from sklearn.linear_model import LogisticRegression
 from collections import Counter
 from torch.nn import BCELoss
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
+from sklearn import metrics
 
 
 class DataTransformer():
@@ -231,28 +227,25 @@ def type_train_and_test(work_dir, train_batch_size=32, epochs=12, lr=2e-5):
     trainer = pl.Trainer(max_epochs=epochs, gpus=1, callbacks=[checkpoint_callback], progress_bar_refresh_rate=30)
     # Train the Classifier Model
     trainer.fit(model, t_data_module)
-    trainer.test(model, datamodule=t_data_module)
+    # trainer.test(model, datamodule=t_data_module)
+    test_dataloader = t_data_module.test_dataloader()
     return model
 
-
-def produce_types(model, context_resource: ContextResources, data_transformer:DataTransformer, ent_emb, ent2id, entid2idx, threshold=0.4):
-    produce_dataset = TypeDataset(ent_emb, ent2id, entid2idx, data_transformer.x, data_transformer.y)
-    pred_sampler = SequentialSampler(produce_dataset)
-    produce_dataloader = DataLoader(produce_dataset, sampler=pred_sampler, batch_size=64)
-    # Put model in evaluation mode
-    model = model.to(device) # moving model to cuda
+def pred(model, dataloader):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     model.eval()
     # Tracking variables
     pred_outs, true_labels = [], []
+    #i=0
     # Predict
-    for batch in produce_dataloader:
+    for batch in dataloader:
         # Add batch to GPU
         batch = tuple(t.to(device) for t in batch)
         # Unpack the inputs from our dataloader
-        b_input_ids, b_labels = batch
+        b_input_ids, b_attn_mask, b_labels = batch
         with torch.no_grad():
             # Forward pass, calculate logit predictions
-            pred_out = model(b_input_ids)
+            pred_out = model(b_input_ids,b_attn_mask)
             pred_out = torch.sigmoid(pred_out)
             # Move predicted output and labels to CPU
             pred_out = pred_out.detach().cpu().numpy()
@@ -263,6 +256,35 @@ def produce_types(model, context_resource: ContextResources, data_transformer:Da
     flat_pred_outs = np.concatenate(pred_outs, axis=0)
     # Combine the correct labels for each batch into a single list.
     flat_true_labels = np.concatenate(true_labels, axis=0)
+    return flat_pred_outs, flat_true_labels
+
+
+def eval_types(model, test_dataloader):
+    flat_pred_outs, flat_true_labels = pred(model, test_dataloader)
+    threshold = np.arange(0.4, 0.51, 0.01)
+    scores = [] # Store the list of f1 scores for prediction on each threshold
+    #convert labels to 1D array
+    y_true = flat_true_labels.ravel()
+    for thresh in threshold:
+        #classes for each threshold
+        pred_bin_label = classify(flat_pred_outs,thresh)
+        #convert to 1D array
+        y_pred = np.array(pred_bin_label).ravel()
+        scores.append(metrics.f1_score(y_true,y_pred))
+    # find the optimal threshold
+    opt_thresh = threshold[scores.index(max(scores))]
+    print(f'Optimal Threshold Value = {opt_thresh}')
+    #predictions for optimal threshold
+    y_pred_labels = classify(flat_pred_outs,opt_thresh)
+    y_pred = np.array(y_pred_labels).ravel() # Flatten
+    print(metrics.classification_report(y_true,y_pred))
+
+
+def produce_types(model, data_transformer:DataTransformer, ent_emb, ent2id, entid2idx, threshold=0.4):
+    produce_dataset = TypeDataset(ent_emb, ent2id, entid2idx, data_transformer.x, data_transformer.y)
+    pred_sampler = SequentialSampler(produce_dataset)
+    produce_dataloader = DataLoader(produce_dataset, sampler=pred_sampler, batch_size=64)
+    flat_pred_outs, flat_true_labels = pred(model, produce_dataloader)
     thresh = np.float(threshold)
     #convert labels to 1D array
     y_true = flat_true_labels.ravel()
@@ -287,71 +309,71 @@ def classify(pred_prob,thresh):
         y_pred.append(temp)
     return y_pred
 
-def node_classification(dataset, checkpoint):
-    ent_emb = torch.load(f'output/ent_emb-{checkpoint}.pt', map_location='cpu')
-    if isinstance(ent_emb, tuple):
-        ent_emb = ent_emb[0]
-
-    ent_emb = ent_emb.squeeze().numpy()
-    num_embs, emb_dim = ent_emb.shape
-    print(f'Loaded {num_embs} embeddings with dim={emb_dim}')
-
-    emb_ids = torch.load(f'output/ents-{checkpoint}.pt', map_location='cpu')
-    ent2idx = utils.make_ent2idx(emb_ids, max_ent_id=emb_ids.max()).numpy()
-    maps = torch.load(f'data/{dataset}/maps.pt')
-    ent_ids = maps['ent_ids']
-    class2labels = defaultdict(lambda: len(class2labels))
-
-    splits = ['train', 'dev', 'test']
-    split_2data = dict()
-    for split in splits:
-        with open(f'data/{dataset}/{split}-ents-class.txt') as f:
-            idx = []
-            labels = []
-            for line in f:
-                entity, ent_class = line.strip().split()
-                entity_id = ent_ids[entity]
-                entity_idx = ent2idx[entity_id]
-                idx.append(entity_idx)
-                labels.append(class2labels[ent_class])
-
-            x = ent_emb[idx]
-            y = np.array(labels)
-            split_2data[split] = (x, y)
-
-    x_train, y_train = split_2data['train']
-    x_dev, y_dev = split_2data['dev']
-    x_test, y_test = split_2data['test']
-
-    best_dev_metric = 0.0
-    best_c = 0
-    for k in range(-4, 2):
-        c = 10 ** -k
-        model = LogisticRegression(C=c, multi_class='multinomial',
-                                   max_iter=1000)
-        model.fit(x_train, y_train)
-
-        dev_preds = model.predict(x_dev)
-        dev_acc = accuracy_score(y_dev, dev_preds)
-        print(f'{c:.3f} - {dev_acc:.3f}')
-
-        if dev_acc > best_dev_metric:
-            best_dev_metric = dev_acc
-            best_c = c
-
-    print(f'Best regularization coefficient: {best_c:.4f}')
-    model = LogisticRegression(C=best_c, multi_class='multinomial',
-                               max_iter=1000)
-    x_train_all = np.concatenate((x_train, x_dev))
-    y_train_all = np.concatenate((y_train, y_dev))
-    model.fit(x_train_all, y_train_all)
-
-    for metric_fn in (accuracy_score, balanced_accuracy_score):
-        train_preds = model.predict(x_train_all)
-        train_metric = metric_fn(y_train_all, train_preds)
-
-        test_preds = model.predict(x_test)
-        test_metric = metric_fn(y_test, test_preds)
-
-        print(f'Train {metric_fn.__name__}: {train_metric:.3f}')
-        print(f'Test {metric_fn.__name__}: {test_metric:.3f}')
+# def node_classification(dataset, checkpoint):
+#     ent_emb = torch.load(f'output/ent_emb-{checkpoint}.pt', map_location='cpu')
+#     if isinstance(ent_emb, tuple):
+#         ent_emb = ent_emb[0]
+#
+#     ent_emb = ent_emb.squeeze().numpy()
+#     num_embs, emb_dim = ent_emb.shape
+#     print(f'Loaded {num_embs} embeddings with dim={emb_dim}')
+#
+#     emb_ids = torch.load(f'output/ents-{checkpoint}.pt', map_location='cpu')
+#     ent2idx = utils.make_ent2idx(emb_ids, max_ent_id=emb_ids.max()).numpy()
+#     maps = torch.load(f'data/{dataset}/maps.pt')
+#     ent_ids = maps['ent_ids']
+#     class2labels = defaultdict(lambda: len(class2labels))
+#
+#     splits = ['train', 'dev', 'test']
+#     split_2data = dict()
+#     for split in splits:
+#         with open(f'data/{dataset}/{split}-ents-class.txt') as f:
+#             idx = []
+#             labels = []
+#             for line in f:
+#                 entity, ent_class = line.strip().split()
+#                 entity_id = ent_ids[entity]
+#                 entity_idx = ent2idx[entity_id]
+#                 idx.append(entity_idx)
+#                 labels.append(class2labels[ent_class])
+#
+#             x = ent_emb[idx]
+#             y = np.array(labels)
+#             split_2data[split] = (x, y)
+#
+#     x_train, y_train = split_2data['train']
+#     x_dev, y_dev = split_2data['dev']
+#     x_test, y_test = split_2data['test']
+#
+#     best_dev_metric = 0.0
+#     best_c = 0
+#     for k in range(-4, 2):
+#         c = 10 ** -k
+#         model = LogisticRegression(C=c, multi_class='multinomial',
+#                                    max_iter=1000)
+#         model.fit(x_train, y_train)
+#
+#         dev_preds = model.predict(x_dev)
+#         dev_acc = accuracy_score(y_dev, dev_preds)
+#         print(f'{c:.3f} - {dev_acc:.3f}')
+#
+#         if dev_acc > best_dev_metric:
+#             best_dev_metric = dev_acc
+#             best_c = c
+#
+#     print(f'Best regularization coefficient: {best_c:.4f}')
+#     model = LogisticRegression(C=best_c, multi_class='multinomial',
+#                                max_iter=1000)
+#     x_train_all = np.concatenate((x_train, x_dev))
+#     y_train_all = np.concatenate((y_train, y_dev))
+#     model.fit(x_train_all, y_train_all)
+#
+#     for metric_fn in (accuracy_score, balanced_accuracy_score):
+#         train_preds = model.predict(x_train_all)
+#         train_metric = metric_fn(y_train_all, train_preds)
+#
+#         test_preds = model.predict(x_test)
+#         test_metric = metric_fn(y_test, test_preds)
+#
+#         print(f'Train {metric_fn.__name__}: {train_metric:.3f}')
+#         print(f'Test {metric_fn.__name__}: {test_metric:.3f}')

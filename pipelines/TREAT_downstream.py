e98ef851aeee1717714e89bc421e6ff4b2be1f23
sylviawangfr <newtyjun@hotmail.com>
2022-05-16 00:44:07 +0800

    fix downstream sampling

--- pipelines/TREAT_downstream.py
+++ pipelines/TREAT_downstream.py
@@ -10,19 +10,22 @@ def TREAT_for_downstream_task(work_dir):
     dataset = "TREAT"
     data_conf = DatasetConfig().get_config(dataset)
     blp_conf = BLPConfig().get_blp_config(rel_model="transe", inductive=False, dataset=dataset, schema_aware=True)
-    p_config = PipelineConfig().set_config(blp_config=blp_conf,
-                                           data_config=data_conf,
-                                           dataset=dataset,
-                                           loops=1,
-                                           work_dir=work_dir,
-                                           use_gpu=True)
-    context_resource, abox_scanner_scheduler = prepare_context(p_config, consistency_check=True,
-                                                               create_id_file=False)
+    p_config = PipelineConfig().set_pipeline_config(dataset=dataset,
+                                                    loops=1,
+                                                    work_dir=work_dir,
+                                                    pred_type=False,
+                                                    reasoner=None,
+                                                    parallel=False,
+                                                    pipeline='l',
+                                                    silver_eval=False,
+                                                    use_gpu=True)
+    p_config.set_blp_config(blp_conf).set_data_config(data_conf)
+    context_resource, abox_scanner_scheduler = prepare_context(p_config, consistency_check=True)
 
     hrt_int_df_2_hrt_blp(context_resource, work_dir,
                          triples_only=False)  # generate all_triples.tsv, entities.txt, relations.txt\
     wait_until_file_is_saved(work_dir + "all_triples.tsv")
-    split_all_triples(context_resource, work_dir, inductive=False,
+    split_data_blp(context_resource=context_resource, work_dir=work_dir, inductive=False,
                       exclude_rels=[])  # split all_triples.tsv to train.tsv, dev.tsv, takes time
     wait_until_blp_data_ready(work_dir, inductive=False)
     # 1. run blp
@@ -38,8 +41,7 @@ def TREAT_for_downstream_task(work_dir):
     # 3. get valid new triples
     to_scan_df = pd.concat([context_resource.hrt_int_df, pred_hrt_df]).drop_duplicates(keep="first").reset_index(
         drop=True)
-    abox_scanner_scheduler.set_triples_to_scan_int_df(to_scan_df).\
-        scan_IJ_patterns(work_dir=work_dir, save_result=True)
+    abox_scanner_scheduler.set_triples_to_scan_int_df(to_scan_df).scan_rel_IJPs(work_dir=work_dir, save_result=True)
 
 
 if __name__ == "__main__":

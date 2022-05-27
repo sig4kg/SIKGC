c46082eec246357c7d40e93e01c6280573a502c6
sylviawangfr <newtyjun@hotmail.com>
2022-05-28 00:33:56 +0800

    set epoch

--- module_utils/type_producer.py
+++ module_utils/type_producer.py
@@ -150,7 +150,7 @@ class TypeDataModule(pl.LightningDataModule):
                                         self.test_label)
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=2)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=64)
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size)

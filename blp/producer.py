d7799ae3cc0d1dd3b03fdc76adab4ed6f273b5db
sylviawangfr <newtyjun@hotmail.com>
2022-06-17 11:47:20 +0800

    add silver  data

--- blp/producer.py
+++ blp/producer.py
@@ -20,7 +20,7 @@ import pandas as pd
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 ex = Experiment(save_git_info=False)
 ex.logger = utils.get_logger()
-NUM_WORKERS = 8
+NUM_WORKERS = 4
 
 
 @ex.config

42787efa569640905487b25ffe1f1e5a22c6e21a
sylviawangfr <newtyjun@hotmail.com>
2022-06-25 15:22:36 +0800

    refactor anyburl

--- pipelines/exp_config.py
+++ pipelines/exp_config.py
@@ -101,7 +101,7 @@ class BLPConfig:
             tmp_conf.update({'model': 'transductive', 'regularizer': 1e-2, 'lr': 1e-3, 'use_scheduler': False})
 
         if dataset == "DBpedia":
-            tmp_conf.update({'batch_size': 64, 'max_epochs': 80})
+            tmp_conf.update({'batch_size': 1024, 'max_epochs': 80})
             if inductive:
                 tmp_conf.update({'lr': 1e-4, 'max_epochs': 80})
         elif dataset == "TREAT":
@@ -109,7 +109,7 @@ class BLPConfig:
             if inductive:
                 tmp_conf.update({'model': 'fasttext', 'lr': 5e-4, 'max_epochs': 60, 'batch_size': 128})
         elif dataset == "NELL":
-            tmp_conf.update({'max_epochs': 80, 'batch_size': 64})
+            tmp_conf.update({'max_epochs': 80, 'batch_size': 1024})
             if inductive:
                 tmp_conf.update({'lr': 1e-4, "max_epochs": 80})
             if rel_model == 'rotate':
